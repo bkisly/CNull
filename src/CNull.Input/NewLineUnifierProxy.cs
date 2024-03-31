1580342c43c31a34cd0a0b07e6a20928be71fb5a
bkisly <b.kisly@gmail.com>
2024-03-31 01:52:53 +0100

    Created a proxy for unifying new line representation

---
+++ src/CNull.Input/NewLineUnifierProxy.cs
@@ -0,0 +1,18 @@
+﻿namespace CNull.Input
+{
+    /// <summary>
+    /// Class responsible for unifying new line representation to '\n' form. Proxy layer between lexer and input.
+    /// </summary>
+    internal class NewLineUnifierProxy(IRawCodeInput input) : ICodeInput
+    {
+        public char? CurrentCharacter => input.CurrentCharacter;
+
+        public void MoveToNext()
+        {
+            input.MoveToNext();
+
+            if(CurrentCharacter == '\r')
+                input.MoveToNext();
+        }
+    }
+}
