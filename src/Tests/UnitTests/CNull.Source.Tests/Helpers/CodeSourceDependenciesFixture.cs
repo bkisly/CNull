5360739ceb72e0aa8faab814f3eb5025183b4444
bkisly <b.kisly@gmail.com>
2024-04-01 20:38:06 +0200

    Created tests for newline proxy

--- src/Tests/UnitTests/CNull.Source.Tests/Helpers/CodeSourceDependenciesFixture.cs
+++ src/Tests/UnitTests/CNull.Source.Tests/Helpers/CodeSourceDependenciesFixture.cs
@@ -3,49 +3,26 @@ using CNull.Source.Repositories;
 
 namespace CNull.Source.Tests.Helpers
 {
-    public class CodeSourceDependenciesFixture
+    public class CodeSourceDependenciesFixture : SourceFixture
     {
         public Mock<IInputRepository> InputRepositoryMock { get; private set; } = new();
         public Mock<ICoreComponentsMediator> MediatorMock { get; private set; } = new();
-
-        public string MockedBuffer { get; set; } = string.Empty;
-
-        private bool EndOfStream => _currentPosition >= MockedBuffer.Length;
-        private int _currentPosition;
 
         public CodeSourceDependenciesFixture()
         {
             Reset();
         }
 
-        public void Reset()
+        public sealed override void Reset()
         {
             InputRepositoryMock = new Mock<IInputRepository>();
             InputRepositoryMock.Setup(s => s.Read())
-                .Returns(() => !EndOfStream ? MockedBuffer[_currentPosition] : -1)
+                .Returns(() => !EndOfBuffer ? MockedBuffer[CurrentPosition] : -1)
                 .Callback(AdvanceStream);
 
             MediatorMock = new Mock<ICoreComponentsMediator>();
-            MockedBuffer = string.Empty;
-            _currentPosition = 0;
-        }
-
-        public static IEnumerable<char?> GetExpectedStreamReads(string buffer, int numberOfReads)
-        {
-            var currentRead = 0;
-
-            for (; currentRead < Math.Min(numberOfReads, buffer.Length); currentRead++)
-                yield return buffer[currentRead++];
-
-            for (; currentRead < numberOfReads; currentRead++)
-                yield return null;
-        }
-
-        private void AdvanceStream()
-        {
-            if (EndOfStream)
-                _currentPosition = MockedBuffer.Length;
-            else _currentPosition++;
+            base.Reset();
+            CurrentPosition = 0;
         }
     }
 }
---
+++ src/Tests/UnitTests/CNull.Source.Tests/Helpers/SourceFixture.cs
@@ -0,0 +1,34 @@
+﻿namespace CNull.Source.Tests.Helpers
+{
+    public class SourceFixture
+    {
+        public string MockedBuffer { get; set; } = string.Empty;
+
+        protected int CurrentPosition;
+        protected bool EndOfBuffer => CurrentPosition >= MockedBuffer.Length;
+
+        public virtual void Reset()
+        {
+            MockedBuffer = string.Empty;
+            CurrentPosition = -1;
+        }
+
+        public virtual IEnumerable<char?> GetExpectedStreamReads(string buffer, int numberOfReads)
+        {
+            var currentRead = 0;
+
+            for (; currentRead < Math.Min(numberOfReads, buffer.Length); currentRead++)
+                yield return buffer[currentRead++];
+
+            for (; currentRead < numberOfReads; currentRead++)
+                yield return null;
+        }
+
+        protected void AdvanceStream()
+        {
+            if (EndOfBuffer)
+                CurrentPosition = MockedBuffer.Length;
+            else CurrentPosition++;
+        }
+    }
+}
