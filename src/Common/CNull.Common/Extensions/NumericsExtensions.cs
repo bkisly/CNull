8a6f58c56dde3cad036900b693953be8733530a6
bkisly <b.kisly@gmail.com>
2024-04-02 23:09:03 +0200

    Created basic implementation of numeric lexer state

---
+++ src/Common/CNull.Common/Extensions/NumericsExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace CNull.Common.Extensions
+{
+    public static class NumericsExtensions
+    {
+        /// <summary>
+        /// Returns the amount of decimal digits in the number.
+        /// </summary>
+        public static int Length(this int i) => i == 0 ? 1 : (int)Math.Log10(Math.Abs(i)) + 1;
+
+        /// <summary>
+        /// Returns the amount of decimal digits in the number.
+        /// </summary>
+        public static long Length(this long l) => l == 0 ? 1 : (long)Math.Log10(Math.Abs(l)) + 1;
+    }
+}
