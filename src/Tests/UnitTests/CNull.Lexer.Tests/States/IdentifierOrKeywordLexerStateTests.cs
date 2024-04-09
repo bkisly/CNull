b2d92fa92d3135558779a9ef98c770814c2b20ee
bkisly <b.kisly@gmail.com>
2024-04-09 19:59:45 +0200

    Refactored state tests

--- src/Tests/UnitTests/CNull.Lexer.Tests/States/IdentifierOrKeywordLexerStateTests.cs
+++ src/Tests/UnitTests/CNull.Lexer.Tests/States/IdentifierOrKeywordLexerStateTests.cs
@@ -7,51 +7,18 @@ namespace CNull.Lexer.Tests.States
     public class IdentifierOrKeywordLexerStateTests(NewLineProxyFixture fixture) : IClassFixture<NewLineProxyFixture>
     {
         [Theory, ClassData(typeof(IdentifiersData))]
-        public void CanBuildIdentifiers(string input, bool succeeded, Token token)
-            => CanBuildIdentifiersOrKeywords(input, succeeded, token);
+        public void CanBuildIdentifiers(string input, bool expectedResult, Token expectedToken)
+            => StateTestsCore.TestTokensCreation(input, expectedResult, expectedToken,
+                new IdentifierOrKeywordLexerState(fixture.CodeSourceMock.Object), fixture);
 
         [Theory, ClassData(typeof(KeywordsData))]
-        public void CanBuildKeywords(string input, bool succeeded, Token token)
-            => CanBuildIdentifiersOrKeywords(input, succeeded, token);
+        public void CanBuildKeywords(string input, bool expectedResult, Token expectedToken)
+            => StateTestsCore.TestTokensCreation(input, expectedResult, expectedToken,
+                new IdentifierOrKeywordLexerState(fixture.CodeSourceMock.Object), fixture);
 
         [Theory, ClassData(typeof(IdentifiersLastCharacterData))]
-        public void CanAdvanceToTheNextTokenCharacterWhenBuilt(string input, char? expectedCurrentCharacter)
-        {
-            // Arrange
-
-            fixture.Reset();
-            fixture.MockedBuffer = input;
-            fixture.CodeSourceMock.Object.MoveToNext();
-
-            var state = new IdentifierOrKeywordLexerState(fixture.CodeSourceMock.Object);
-
-            // Act
-
-            state.TryBuildToken(out _);
-
-            // Assert
-
-            Assert.Equal(expectedCurrentCharacter, fixture.CodeSourceMock.Object.CurrentCharacter);
-        }
-
-        private void CanBuildIdentifiersOrKeywords(string input, bool succeeded, Token token)
-        {
-            // Arrange
-
-            fixture.Reset();
-            fixture.MockedBuffer = input;
-            fixture.CodeSourceMock.Object.MoveToNext();
-
-            var state = new IdentifierOrKeywordLexerState(fixture.CodeSourceMock.Object);
-
-            // Act
-
-            var result = state.TryBuildToken(out var resultToken);
-
-            // Assert
-
-            Assert.Equal(succeeded, result);
-            Assert.Equivalent(token, resultToken);
-        }
+        public void CanFinishAtProperCharacter(string input, char? expectedCharacter)
+            => StateTestsCore.TestFinishedCharacter(input, expectedCharacter,
+                new IdentifierOrKeywordLexerState(fixture.CodeSourceMock.Object), fixture);
     }
 }
--- src/Tests/UnitTests/CNull.Lexer.Tests/States/NumericLexerStateTests.cs
+++ src/Tests/UnitTests/CNull.Lexer.Tests/States/NumericLexerStateTests.cs
@@ -7,51 +7,18 @@ namespace CNull.Lexer.Tests.States
     public class NumericLexerStateTests(NewLineProxyFixture fixture) : IClassFixture<NewLineProxyFixture>
     {
         [Theory, ClassData(typeof(IntegerLiteralsData))]
-        public void CanBuildIntegerLiterals(string input, bool expectedValid, Token expectedToken)
-            => CanBuildNumericLiterals(input, expectedValid, expectedToken);
+        public void CanBuildIntegerLiterals(string input, bool expectedResult, Token expectedToken)
+            => StateTestsCore.TestTokensCreation(input, expectedResult, expectedToken,
+                new NumericLexerState(fixture.CodeSourceMock.Object), fixture);
 
         [Theory, ClassData(typeof(FloatingPointLiteralsData))]
-        public void CanBuildFloatingPointLiterals(string input, bool expectedValid, Token expectedToken)
-            => CanBuildNumericLiterals(input, expectedValid, expectedToken);
+        public void CanBuildFloatingPointLiterals(string input, bool expectedResult, Token expectedToken)
+            => StateTestsCore.TestTokensCreation(input, expectedResult, expectedToken,
+                new NumericLexerState(fixture.CodeSourceMock.Object), fixture);
 
         [Theory, ClassData(typeof(NumericStateFinishingCharacterData))]
-        public void CanFinishAtProperCharacter(string input, char? expectedCurrentCharacter)
-        {
-            // Arrange
-
-            fixture.Reset();
-            fixture.MockedBuffer = input;
-            fixture.CodeSourceMock.Object.MoveToNext();
-
-            var state = new NumericLexerState(fixture.CodeSourceMock.Object);
-
-            // Act
-
-            state.TryBuildToken(out _);
-
-            // Assert
-
-            Assert.Equal(expectedCurrentCharacter, fixture.CodeSourceMock.Object.CurrentCharacter);
-        }
-
-        private void CanBuildNumericLiterals(string input, bool expectedValid, Token expectedToken)
-        {
-            // Arrange
-
-            fixture.Reset();
-            fixture.MockedBuffer = input;
-            fixture.CodeSourceMock.Object.MoveToNext();
-
-            var state = new NumericLexerState(fixture.CodeSourceMock.Object);
-
-            // Act
-
-            var result = state.TryBuildToken(out var token);
-
-            // Assert
-
-            Assert.Equal(expectedValid, result);
-            Assert.Equivalent(expectedToken, token);
-        }
+        public void CanFinishAtProperCharacter(string input, char? expectedCharacter)
+            => StateTestsCore.TestFinishedCharacter(input, expectedCharacter,
+                new NumericLexerState(fixture.CodeSourceMock.Object), fixture);
     }
 }
